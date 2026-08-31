cbfa1fe1caa9d5d123f908aa53cad33d9a3f515d
dcbarros <dcbarros1@gmail.com>
2026-08-31 17:13:17 -0300

    refactor: adding diferent db to tests enviroments

--- app/database.py
+++ app/database.py
@@ -1,8 +1,13 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 
-DATABASE_URL = "sqlite:///./task_pilot.db"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///./task_pilot.db"
+)
 
 
 class Base(DeclarativeBase):
